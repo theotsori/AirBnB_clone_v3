192ad571c90b3318c8e5f4930cc831f8220bcbd9
theotsori <theodoretsori@gmail.com>
2023-04-29 10:20:33 +0300

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Flask api app """
 
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Api index file """
 
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
