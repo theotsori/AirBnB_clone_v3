c86d117e42109b5f4da2b709ad0d0a55696e86b4
theotsori <theodoretsori@gmail.com>
2023-05-01 10:33:11 +0300

    User v1.0

---
+++ api/v1/views/users.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""User objects that handles all default RESTFul API actions"""
+
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_all_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all(User).values()
+    users_list = [user.to_dict() for user in users]
+    return jsonify(users_list)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a User object"""
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'email' not in request.json:
+        abort(400, 'Missing email')
+    if 'password' not in request.json:
+        abort(400, 'Missing password')
+    user = User(**request.get_json())
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in request.get_json().items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
