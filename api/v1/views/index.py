3103cefc3268754112c9a60ac4897625f91be5d8
theotsori <theodoretsori@gmail.com>
2023-05-02 11:40:13 +0300

    Status of your API v1.0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """ Api status output """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/api/v1/stats', methods=['GET'])
 def stats():
